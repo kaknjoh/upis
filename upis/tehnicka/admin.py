a9eb362fe42175679b0fe90130d25bd44d049cce
Harun <harun@eacon.ba>
2019-12-29 16:42:06 +0100

    Add admin.py

--- upis/tehnicka/admin.py
+++ upis/tehnicka/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Predmet,Ucenik,Predmet_Ocjena,Priznanja,Skola,Smjer
 
 # Register your models here.
+admin.site.register(Predmet)
+admin.site.register(Ucenik)
+admin.site.register(Predmet_Ocjena)
+admin.site.register(Priznanja)
+admin.site.register(Skola)
+admin.site.register(Smjer)
+admin.site.site_header = 'Upis ucenika'
+admin.site.index_title = 'Tehnicka skola Zenica'
+admin.site.site_title = 'Upis ucenika u skolu' 
