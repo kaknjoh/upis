4e5012765d28b2223061df4e4fff7cc8f0ada106
Harun <harun@eacon.ba>
2020-01-02 13:28:26 +0100

    Dodana funkcija index i fija u njoj statistika

--- upis/tehnicka/views.py
+++ upis/tehnicka/views.py
@@ -1,19 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from .models import  Predmet,Ucenik,Predmet_Ocjena,Priznanja,Skola,Smjer
+from .models import  Predmet,Ucenik,Predmet_Ocjena,Priznanja,Smjer,Skola
 from django.contrib.auth import authenticate, login, logout
 
 import json
 from .createUcenik import *
+from .statistika import *
 
-def index_view(request):
-    smjerovi=Smjer.objects.all()
-    skole=Skola.objects.all()
-    context={
-        'smjerovi':smjerovi,
-        'skole':skole
-        }
-    return render(request, 'tehnicka/base.html',context)
+
 
 
 def index_viev_predmet(request):
@@ -22,12 +16,28 @@ def index_viev_predmet(request):
         'predmeti':predmeti
         }
     return render(request,'tehnicka/dodajUcenika.html',context)
+
+
 # Create your views here.
+def index(request,smjer_id):
+    message=''
+    if request.session.get('message'):
+        message='Korisnik obrisan!'
+        del request.session['message']
+    return racunajStatistiku(request, smjer_id, message)
 
 def dodajucenika(request):
         if request.method == "POST":
             try:
-                return saveStudent(request)
+                #predmet_razred9_ocjena=request.POST.getlist('razred9')
+                #predmeti_razred7_ocjena=request.POST.getlist('razred7')
+                #context={
+                    #'ocjene':predmet_razred9_ocjena
+                   # }
+                #return render(request,'tehnicka/dodajUcenika.html',context)
+                saveStudent(request)
+                response=redirect('/admin/')
+                return response
             except KeyError:
                
                 return HttpResponse(" Error dodajucenika")
